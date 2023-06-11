cb8b2e1fd0e339b66c1d622146d10abfd9d2354a
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-11 13:52:57 +0530

    element function.selected..

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_ssm_parameter" "params" {
   count = length(var.params)
   name  = var.params[count.index].name
+  type = var.params[count.index].name
   value = var.params[count.index].value
 }
 variable "params" {
