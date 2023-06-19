9acb9d2f03932c97b6ead1abd15675faa98d386c
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-19 20:32:34 +0530

    terraform basics

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ resource "aws_ssm_parameter" "params" {
 }
 variable "params" {
   default = [
-    { name = "roboshop.dev.fronted.catalogue_url", value = "http://172.31.93.98:8080", type = "String" },
-    { name = "roboshop.dev.fronted.user_url", value = "http://172.31.95.5:8080", type = "String" },
-    { name = "roboshop.dev.fronted.cart_url", value = "http://172.31.93.136:8080", type = "String" },
-    { name = "roboshop.dev.fronted.payment_url", value = "http://172.31.85.181:8080", type = "String" },
-    { name = "roboshop.dev.fronted.shipping_url", value = "http://172.31.92.27:8080", type = "String" },
+    { name = "roboshop.dev.fronted.catalogue_url", value = "http://172.31.93.98:8080/", type = "String" },
+    { name = "roboshop.dev.fronted.user_url", value = "http://172.31.95.5:8080/", type = "String" },
+    { name = "roboshop.dev.fronted.cart_url", value = "http://172.31.93.136:8080/", type = "String" },
+    { name = "roboshop.dev.fronted.payment_url", value = "http://172.31.85.181:8080/", type = "String" },
+    { name = "roboshop.dev.fronted.shipping_url", value = "http://172.31.92.27:8080/", type = "String" },
 
   ]
 }
