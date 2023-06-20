97c568dc689eceea78972d005aa9b2cbc6b2c9c5
bhagyapolaboina <bhagyapolaboina@gmail.com>
2023-06-20 11:06:03 +0530

    terraform basics

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ variable "params" {
     { name = "roboshop.dev.fronted.user_url", value = "http://172.31.95.5:8080/", type = "String" },
     { name = "roboshop.dev.fronted.cart_url", value = "http://172.31.93.136:8080/", type = "String" },
     { name = "roboshop.dev.fronted.payment_url", value = "http://172.31.85.181:8080/", type = "String" },
-    { name = "roboshop.dev.fronted.shipping_url", value = "http://172.31.92.27:8080/", type = "String" },
+    { name = "roboshop.dev.fronted.shipping_url", value = "http://172.31.92.27:8080/", type = "String" }
 
   ]
 }
